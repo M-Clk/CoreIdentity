e99cd34411915cfa1dda3a0e7eff34634c462568
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-01-12 02:44:38 +0300

    Added updating user.

--- CoreIdentity/Controllers/AdminController.cs
+++ CoreIdentity/Controllers/AdminController.cs
@@ -11,9 +11,13 @@ namespace CoreIdentity.Controllers
     public class AdminController : Controller
     {
         private UserManager<ApplicationUser> userManager;
-        public AdminController(UserManager<ApplicationUser> _userManager)
+        private IPasswordValidator<ApplicationUser> passwordValidator;
+        private IPasswordHasher<ApplicationUser> passwordHasher;
+        public AdminController(UserManager<ApplicationUser> _userManager, IPasswordValidator<ApplicationUser> _passwordValidator, IPasswordHasher<ApplicationUser> _passwordHasher)
         {
             userManager = _userManager;
+            passwordValidator = _passwordValidator;
+            passwordHasher = _passwordHasher;
         }
         public IActionResult Index()
         {
@@ -45,10 +49,10 @@ namespace CoreIdentity.Controllers
         public async Task<IActionResult> Delete(string Id)
         {
             var user = await userManager.FindByIdAsync(Id);
-            if(user!=null)
+            if (user != null)
             {
                 var result = await userManager.DeleteAsync(user);
-                if(result.Succeeded)
+                if (result.Succeeded)
                     return RedirectToAction("Index");
                 else
                     foreach (var item in result.Errors)
@@ -58,5 +62,47 @@ namespace CoreIdentity.Controllers
                 ModelState.AddModelError("", "User not found!");//keyi bos olan hatalar model hatalaridir.
             return View("Index", userManager.Users);
         }
+        [HttpGet]
+        public async Task<IActionResult> Update(string Id)
+        {
+            var user = await userManager.FindByIdAsync(Id);
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(string Id, string Password, string Email)
+        {
+            var user = await userManager.FindByIdAsync(Id);
+            if (user != null)
+            {
+                user.Email = Email;
+                IdentityResult validPassword = null;
+
+                if (!string.IsNullOrEmpty(Password))
+                {
+                    validPassword = await passwordValidator.ValidateAsync(userManager, user, Password);
+                    if (validPassword.Succeeded)
+                        user.PasswordHash = passwordHasher.HashPassword(user, Password);
+                    else
+                        foreach (var item in validPassword.Errors)
+                            ModelState.AddModelError("", item.Description);
+                }
+
+                if (validPassword==null || validPassword.Succeeded)
+                {
+                    var result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else foreach (var item in result.Errors)
+                            ModelState.AddModelError("", item.Description);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User not found!");
+            return View(user);
+        }
+
     }
 }
